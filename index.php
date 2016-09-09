0482e895bd4129422c8d57961b30b7a338a279b9
Dev2_Luan <luan@suldopara.com.br>
2016-09-09 14:17:06 -0300

    Ajustes em Layout - Index

---
+++ index.php
@@ -0,0 +1,326 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Grupo de desenvolvedores de PHP do estado do Pará | PHPPA.ORG</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Grupo de desenvolvedores de PHP do estado do Pará" />
+
+<script type="application/x-javascript"> 
+		addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); }
+</script>
+<!-- //for-mobile-apps -->
+<link href="public/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="public/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="public/js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+</head>
+	
+<body>
+<!-- header -->
+	<div class="header">
+		<div class="container">
+			<div class="header-nav">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Navegação</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="logo">
+							<a class="navbar-brand" href="index.html">PHPPA.ORG <span>Grupo de desenvolvedores de PHP do estado do Pará</span></a>
+						</div>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					 <ul class="nav navbar-nav cl-effect-14">
+						<li><a href="index.html" class="active">Início</a></li>
+						<li><a href="services.html">Como Contribuir</a></li>
+						<li><a href="codes.html">Quem Somos</a></li>
+						<li><a href="blog.html">Artigos</a></li>
+						<li><a href="contact.html">Contato</a></li>
+					  </ul>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+			</div>
+		</div>
+	</div>
+<!-- //header -->
+<!-- banner -->
+	<div class="banner">
+		<div class="container">
+			<div class="banner-left">
+				<img src="public/images/elefante_t.png" alt=" " class="img-responsive" />
+			</div>
+			<div class="banner-info">
+				<h1><span>Uma <b>comunidade</b> de Desenvolvedores <b>PHP</b> do estado do Pará</span></h1>
+				<h2>Una-se a esta comunidade</h2>
+				
+			</div>
+			<div class="clearfix"> </div>
+			<div class="banner-pos">
+				<ul id="flexiselDemo1">			
+					<li>
+						<div class="banner-pos-grid1">
+							<div class="col-xs-4 banner-pos-grid1-left">
+								<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+							</div>
+							<div class="col-xs-8 banner-pos-grid1-right">
+								<h3>Paixão</h3>
+							</div>
+							<div class="clearfix"> </div>
+							<p>A paixão que nos envolve é a de sempre aprender, ensinar, conversar e debater sobre PHP</p>
+						</div>
+					</li>
+					<li>
+						<div class="banner-pos-grid1 banner-pos-grid2">
+							<div class="col-xs-4 banner-pos-grid1-left">
+								<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+							</div>
+							<div class="col-xs-8 banner-pos-grid1-right">
+								<h3>Objetivo</h3>
+							</div>
+							<div class="clearfix"> </div>
+							<p>O objetivo da comunidade de desenvolvedores PHPPA é difundir a linguagem PHP por todo estado do Pará,
+							aproximar programadores de nossa região, unir forças, trocar experiencias, ideias, 
+							realizando eventos para promover networking e troca de insformações.</p>
+						</div>
+					</li>
+					<li>
+						<div class="banner-pos-grid1 banner-pos-grid3">
+							<div class="col-xs-4 banner-pos-grid1-left">
+								<span class="glyphicon glyphicon-fire" aria-hidden="true"></span>
+							</div>
+							<div class="col-xs-8 banner-pos-grid1-right">
+								<h3>Quem é a Comunidade</h3>
+							</div>
+							<div class="clearfix"> </div>
+							<p>Todos que de alguma forma ajudam e participam da comunidade. Você que aparece nos encontros,
+							postam artigos, ajuda no site, nos projetos, códigos, redes sociais ou que comentam nas redes sociais.</p>
+						</div>
+					</li>
+				</ul>
+						<script type="text/javascript">
+							$(window).load(function() {
+								$("#flexiselDemo1").flexisel({
+									visibleItems: 3,
+									animationSpeed: 1000,
+									autoPlay: false,
+									autoPlaySpeed: 3000,    		
+									pauseOnHover: true,
+									enableResponsiveBreakpoints: true,
+									responsiveBreakpoints: { 
+										portrait: { 
+											changePoint:480,
+											visibleItems: 1
+										}, 
+										landscape: { 
+											changePoint:640,
+											visibleItems:2
+										},
+										tablet: { 
+											changePoint:768,
+											visibleItems: 2
+										}
+									}
+								});
+								
+							});
+					</script>
+					<script type="text/javascript" src="public/js/jquery.flexisel.js"></script>
+			</div>
+		</div>
+	</div>
+<!-- //banner -->
+<!-- events -->
+	<div class="events">
+		<div class="container">
+			<div class="events-grids">
+				<div class="col-md-4 events-grid">
+					<div class="mnth-date">
+						<div class="col-xs-5 mnth-date-left">
+							<h4>15 th<span>September</span></h4>
+						</div>
+						<div class="col-xs-7 mnth-date-right">
+							<p>Lorem ipsum dolor sit amet aperiam.</p>
+						</div>
+						<div class="clearfix"> </div>
+						<p class="quasi">Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+							accusantium doloremque laudantium, totam rem aperiam.</p>
+						<div class="more m1">
+							<a href="single.html" class="hvr-curl-bottom-right">Learn More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 events-grid">
+					<div class="mnth-date">
+						<div class="col-xs-5 mnth-date-left">
+							<h4>18 th<span>September</span></h4>
+						</div>
+						<div class="col-xs-7 mnth-date-right">
+							<p>Lorem ipsum dolor sit amet aperiam.</p>
+						</div>
+						<div class="clearfix"> </div>
+						<p class="quasi">Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+							accusantium doloremque laudantium, totam rem aperiam.</p>
+						<div class="more m1">
+							<a href="single.html" class="hvr-curl-bottom-right">Learn More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 events-grid">
+					<div class="mnth-date">
+						<div class="col-xs-5 mnth-date-left">
+							<h4>30 th<span>September</span></h4>
+						</div>
+						<div class="col-xs-7 mnth-date-right">
+							<p>Lorem ipsum dolor sit amet aperiam.</p>
+						</div>
+						<div class="clearfix"> </div>
+						<p class="quasi">Sed ut perspiciatis unde omnis iste natus error sit voluptatem 
+							accusantium doloremque laudantium, totam rem aperiam.</p>
+						<div class="more m1">
+							<a href="single.html" class="hvr-curl-bottom-right">Learn More</a>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="events-grids-bottom">
+				<div class="col-md-4 events-grids-btm">
+					<div class="col-xs-4 events-grids-btm-left">
+						<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+					</div>
+					<div class="col-xs-8 events-grids-btm-right">
+						<h4>6075</h4>
+						<p>Happy Clients</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-4 events-grids-btm">
+					<div class="col-xs-4 events-grids-btm-left">
+						<span class="glyphicon glyphicon-tags" aria-hidden="true"></span>
+					</div>
+					<div class="col-xs-8 events-grids-btm-right">
+						<h4>30</h4>
+						<p>Services Per Month</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-4 events-grids-btm">
+					<div class="col-xs-4 events-grids-btm-left">
+						<span class="glyphicon glyphicon-check" aria-hidden="true"></span>
+					</div>
+					<div class="col-xs-8 events-grids-btm-right">
+						<h4>3000</h4>
+						<p>Customer Satisfaction</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //events -->
+<!-- banner-bottom -->
+	<div class="banner-bottom">
+		<div class="container">
+			<div class="wmuSlider example1">
+			    <div class="wmuSliderWrapper">
+				    <article style="position: absolute; width: 100%; opacity: 0;"> 
+						<div class="banner-wrap">
+							<div class="banner-bottom-info">
+								<h3>Ambiente de desenvolvimento PHP</h3>
+								<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+									dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+									sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+							</div>
+						</div>
+					</article>
+					
+				</div>
+			</div>
+				<script src="public/js/jquery.wmuSlider.js"></script> 
+					  <script>
+						$('.example1').wmuSlider();         
+					 </script> 
+		</div>
+	</div>
+<!-- //banner-bottom -->
+<!-- testimonials -->
+	<div class="testimonials">
+		<div class="container">
+			<div class="testimonials-grids">
+				<center>
+				<p style="color: #444;">Hospedado por:</p><br>
+				<a href="http://www.hosthp.com.br" target="_blank"><img src="public/images/hosthp.png"></a>
+				</center>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+<!-- //testimonials -->
+<!-- footer -->
+	<div class="footer">	
+		<div class="container">
+			<div class="footer-grids">
+				<div class="col-md-3 footer-grid">
+					<h3 class="title"></h3>
+					<ul>
+						<li><a href="#">Como Contribuir</a></li>
+						<li><a href="#">Quem Somos</a></li>
+						<li><a href="#">Artigos</a></li>
+						<li><a href="#">Eventos</a></li>
+						<li><a href="#">Contato</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 footer-grid ctact-grid">
+					
+				</div>
+				<div class="col-md-3 footer-grid ctact-grid">
+					
+				</div>
+				<div class="col-md-3 footer-grid contact-grid">
+						<h3 class="title">Contato</h3>
+						<ul>
+							<li><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><a href="mailto:contato@phppa.org">contato@phppa.org</a></li>
+						</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<div class="copy">
+		<div class="container">
+			<p>Copyright © <?php echo date("Y"); ?>. Design by <a target="_blank" href="http://w3layouts.com">W3layouts</a></p>
+			<div class="social-icons">
+				<ul>
+					<li><a href="#" class="fb"></a></li>
+					<li><a href="#"></a></li>
+					<li><a href="#" class="gg"></a></li>
+					<li><a href="#" class="pn"></a></li>					
+				</ul>	
+			</div>
+		</div>
+	</div>
+<!--//footer-->
+<!-- for bootstrap working -->
+		<script src="public/js/bootstrap.js"> </script>
+<!-- //for bootstrap working -->
+</body>
+</html>
